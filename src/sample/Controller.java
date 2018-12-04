215c285998199a16b1302ae3260d7957634c7c83
emilanovic1 <emilanovic1@etf.unsa.ba>
2018-12-04 11:37:16 +0100

    dodala combobox, choisebox i jednu validaciju

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,23 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML private ChoiceBox choisebox;
-
-
+    @FXML private ChoiceBox odsjek;
+    @FXML private ChoiceBox godina;
+    @FXML private ComboBox  mjesto;
+    @FXML private TextField imeField;
     @Override
     public void initialize(URL url, ResourceBundle rb){
 
@@ -21,9 +27,29 @@ public class Controller implements Initializable {
         choisebox.getItems().add("master");
         choisebox.getItems().add("doktorski studij");
         choisebox.getItems().add("stručni studij");
+        odsjek.setValue("RI");
+        odsjek.getItems().addAll("AiE","RI","TK","EE");
+        godina.setValue("prva");
+        godina.getItems().addAll("prva","druga","treća");
+
+        mjesto.getItems().addAll("Sarajevo","Zenica","Visoko","Tuzla","Mostar");
+        imeField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (validnoIme(n)) {
+                    imeField.getStyleClass().removeAll("poljeNijeIspravno");
+                    imeField.getStyleClass().add("poljeIspravno");
+                } else {
+                    imeField.getStyleClass().removeAll("poljeIspravno");
+                    imeField.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
 
 
-
+    }
+    boolean validnoIme(String n){
+        return true;
     }
 
 }
